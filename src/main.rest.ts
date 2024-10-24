e7c3cfe58bc57ec44cce4ac01445db34511b0e5b
Mariya-Lisachenko <92264453+Mariya-Lisachenko@users.noreply.github.com>
2024-10-25 00:58:49 +0500

    add first and second task

---
+++ src/main.rest.ts
@@ -0,0 +1,8 @@
+import { PinoLogger } from './shared/libs/logger/index.js';
+import { Application } from './rest/index.js';
+async function bootstrap() {
+  const logger = new PinoLogger();
+  const application = new Application(logger);
+  await application.init();
+}
+bootstrap();
---
+++ src/rest/application.ts
@@ -0,0 +1,10 @@
+import { Logger } from '../shared/libs/logger/index.js';
+export class Application {
+  constructor(
+    private readonly logger: Logger
+  ) {}
+
+  public async init() {
+    this.logger.info('Application is initializing');
+  }
+}
