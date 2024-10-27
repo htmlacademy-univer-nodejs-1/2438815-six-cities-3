3858a23784af900c67f9d2bdd060305d6358d887
Mariya-Lisachenko <92264453+Mariya-Lisachenko@users.noreply.github.com>
2024-10-27 21:50:54 +0500

    add singletoneScope

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -7,9 +7,9 @@ import { Component } from './shared/types/index.js';
 
 async function bootstrap() {
   const container = new Container();
-  container.bind<Application>(Component.Application).to(Application);
-  container.bind<PinoLogger>(Component.Logger).to(PinoLogger);
-  container.bind<Config<RestSchema>>(Component.Config).to(RestConfig);
+  container.bind<Application>(Component.Application).to(Application).inSingletonScope();
+  container.bind<PinoLogger>(Component.Logger).to(PinoLogger).inSingletonScope();
+  container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
   const application = container.get<Application>(Component.Application);
   await application.init();
 }
