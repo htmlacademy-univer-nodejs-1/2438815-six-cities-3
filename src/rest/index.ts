ccd4a4ca073d71c40671a5fa242c7e785c6ebef3
Mariya-Lisachenko <92264453+Mariya-Lisachenko@users.noreply.github.com>
2025-01-02 23:35:48 +0500

    add validation exception-filter

--- src/rest/index.ts
+++ src/rest/index.ts
@@ -3,7 +3,7 @@ export * from './container.js';
 export * from './controller-infra/controller.interface.js';
 export * from './controller-infra/base-controller.abstract.js';
 export * from './exception-filter/exception-filter.interface.js';
-export * from './exception-filter/app-exception-filter.js';
+export * from './exception-filter/app.exception-filter.js';
 export * from './types/request-parts.type.js';
 export * from './types/http-method.enum.js';
 export * from './errors/http-error.js';
--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -10,3 +10,6 @@ export * from './component.enum.js';
 export * from './user.type.js';
 export * from './city-names.enum.js';
 export * from './document-exists.interface.js';
+export * from './validation-error-field.type.js';
+export * from './application-error.enum.js';
+
